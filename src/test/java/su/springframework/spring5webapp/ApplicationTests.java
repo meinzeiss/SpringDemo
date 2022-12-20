ba9149a2bfebb90eb7983d2c63ae2fa6195ca328
su <meinzeiss@gmail.com>
2022-12-20 18:37:11 -0400

    initial spring

---
+++ src/test/java/su/springframework/spring5webapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package su.springframework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
